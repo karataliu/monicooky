c7b27bccc4a23f2833e07121195cdc5db5f861d7
Dong Liu <doliu@microsoft.com>
2016-06-02 18:17:19 +0800

    support report rg count.

--- c.ts
+++ c.ts
@@ -9,18 +9,41 @@ function delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+
+import lib = require('./a');
+
+function errlog(data) {
+    console.error("error:" + data);
+    process.exit(1);
+}
+
+function count(data) {
+    var arr = data['value'];
+    return arr.length;
+}
+
+let conf = require('./conf.json');
+let path = conf.path;
+let options: lib.McClientOptions = conf;
+let client = new lib.McClient(options);
+
+
 runWithDelay(3, function () {
     const spawn = require('child_process').spawn;
-    const ls = spawn('zabbix_sender', ['-c', '/etc/zabbix/zabbix_agentd.conf', '-k', 'azure.rgcount', '-o', '5']);
-    ls.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-    });
 
-    ls.stderr.on('data', (data) => {
-        console.log(`stderr: ${data}`);
-    });
+    client.get(path).catch(errlog).then(count).then(function (cou) {
+        console.log('result:' + cou);
+        const ls = spawn('zabbix_sender', ['-c', '/etc/zabbix/zabbix_agentd.conf', '-k', 'azure.rgcount', '-o', cou]);
+        ls.stdout.on('data', (data) => {
+            console.log(`stdout: ${data}`);
+        });
 
-    ls.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
+        ls.stderr.on('data', (data) => {
+            console.log(`stderr: ${data}`);
+        });
+
+        ls.on('close', (code) => {
+            console.log(`child process exited with code ${code}`);
+        });
     });
 });
