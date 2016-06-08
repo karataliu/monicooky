c34caad99b64a448d70fcbd0b2c665bb2651d21b
Dong Liu <doliu@microsoft.com>
2016-06-08 16:12:01 +0800

    support cmdline parameters.

--- src/run.ts
+++ src/run.ts
@@ -4,19 +4,47 @@ import { McUtil } from './mcUtil';
 import { McAdapter } from './adapter';
 import { McClientOptions, IMcQueryEntry } from './common';
 
-const conf: any = McUtil.LoadFile(__dirname + '/conf.json');
-const query: any = McUtil.LoadFile(__dirname + '/query.json');
+const argv = require('minimist')(process.argv.slice(2));
+const confPath = argv['c'] || __dirname + '/conf.json';
+const queryTemplatePath = argv['t'] || __dirname + '/query.json';
+let action = 'help';
+if (argv['_'].length > 0) {
+    action = argv['_'][0];
+}
 
-let queries: IMcQueryEntry[] = query.list || [];
-let options: McClientOptions = conf;
-const client = new mcClient.McClient(options);
-const mcLib = new McLib(client);
-const mcAdapter = new McAdapter(mcLib);
+switch (action) {
+    case 'discovery':
+        getAdapter().GetSubscriptionsDiscovery().then(JSON.stringify).then(console.log).catch(console.error);
+        break;
+    case 'query':
+        const mcAdapter = getAdapter();
+        const query: any = McUtil.LoadFile(queryTemplatePath);
+        let queries: IMcQueryEntry[] = query.list || [];
+        let subsId: string[] = [];
+        if (argv['s']) {
+            subsId = argv['s'].split(',');
+        }
 
-queries = McUtil.ExpandMacro(queries, "{subscriptionId}", ["s1"]);
+        queries = McUtil.ExpandMacro(queries, "{subscriptionId}", subsId);
+        mcAdapter.GetQueryOutput(queries).then(list => {
+            for (let item of list) {
+                console.log(item);
+            }
+        }).catch(console.error);
+        break;
+    case 'help':
+    default:
+        console.log("node run.ts <help|discovery|query> [-c conf.json] [-q queryTempalte.json] [-s subsId1,subsId2]");
+}
 
-mcAdapter.GetQueryOutput(queries).then(list => {
-    for (let item of list) {
-        console.log(item);
-    }
-}).catch(console.error);
+function getAdapter(): McAdapter {
+    const conf: any = McUtil.LoadFile(confPath);
+    const options: McClientOptions = conf;
+    const client = new mcClient.McClient(options);
+    const mcLib = new McLib(client);
+    return new McAdapter(mcLib);
+}
+
+
+
+
