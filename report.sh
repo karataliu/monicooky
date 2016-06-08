9470fd56fcc8b629702a046d5a56223eb775ad7a
Dong Liu <doliu@microsoft.com>
2016-06-08 16:58:10 +0800

    Add vm count, fix report.sh.

--- report.sh
+++ report.sh
@@ -2,5 +2,7 @@
 
 NODE_BIN=/usr/sbin/node
 CUR=$(dirname $0)
-$NODE_BIN $CUR/src/run.js query -s $@| zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -i -
+sh -c "$NODE_BIN $CUR/src/run.js query -s $@| zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -i -" >/dev/null 2>&1 &
+
+echo 1
 
