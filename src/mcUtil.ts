89c0562f8564b3365fc05a12c3fa6d606678fe71
Dong Liu <doliu@microsoft.com>
2016-06-08 15:33:37 +0800

    Add expand macro.

--- src/mcUtil.ts
+++ src/mcUtil.ts
@@ -1,4 +1,4 @@
-import { McClientOptions, IMcResultEntry } from './common';
+import { McClientOptions, IMcResultEntry, IMcQueryEntry} from './common';
 import fs = require('fs');
 
 export class McUtil {
@@ -13,5 +13,29 @@ export class McUtil {
         }
 
         return conf;
+    }
+
+    static ExpandMacro(input: IMcQueryEntry[], macro: string, values: string[]): IMcQueryEntry[] {
+        const output: IMcQueryEntry[] = [];
+        for (let item of input) {
+            if (item.name.indexOf(macro) === -1) {
+                output.push(item);
+                continue;
+            }
+
+            for (let value of values) {
+                let newItem: IMcQueryEntry = {
+                    name: item.name.replace(macro, value),
+                    path: item.path.replace(macro, value),
+                };
+                if (item.query) {
+                    newItem.query = item.query;
+                }
+
+                output.push(newItem);
+            }
+        }
+
+        return output;
     }
 }
--- test/mcUtil.spec.ts
+++ test/mcUtil.spec.ts
@@ -1,1 +1,35 @@
 import { McUtil } from '../src/mcUtil';
+
+const input = [
+    {
+        name: "n1",
+        path: "p1"
+    },
+    {
+        name: "check[{sub1}]",
+        path: "/subscriptions/{sub1}/resourceGroups?api-version=2014-04-01"
+    },
+];
+
+
+describe("test Util", () => {
+    it("test1", () => {
+        let newInput = McUtil.ExpandMacro(input, "{sub1}", ["a1", "b2"]);
+        expect(newInput).toEqual(
+            [
+                {
+                    name: "n1",
+                    path: "p1",
+                },
+                {
+                    name: "check[a1]",
+                    path: "/subscriptions/a1/resourceGroups?api-version=2014-04-01",
+                },
+                {
+                    name: "check[b2]",
+                    path: "/subscriptions/b2/resourceGroups?api-version=2014-04-01",
+                },
+            ]
+        );
+    });
+});
