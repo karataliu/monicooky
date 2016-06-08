72231124b6e910f5eb7f9be84b5d99d5c32c3d44
Dong Liu <doliu@microsoft.com>
2016-06-08 15:41:59 +0800

    split conf and query.

--- src/run.ts
+++ src/run.ts
@@ -4,12 +4,16 @@ import { McUtil } from './mcUtil';
 import { McAdapter } from './adapter';
 import { McClientOptions, IMcQueryEntry } from './common';
 
-let conf: any = McUtil.LoadFile(__dirname + '/conf.json');
-let queries: IMcQueryEntry[] = conf.list || [];
+const conf: any = McUtil.LoadFile(__dirname + '/conf.json');
+const query: any = McUtil.LoadFile(__dirname + '/query.json');
+
+let queries: IMcQueryEntry[] = query.list || [];
 let options: McClientOptions = conf;
-let client = new mcClient.McClient(options);
+const client = new mcClient.McClient(options);
 const mcLib = new McLib(client);
 const mcAdapter = new McAdapter(mcLib);
+
+queries = McUtil.ExpandMacro(queries, "{subscriptionId}", ["s1"]);
 
 mcAdapter.GetQueryOutput(queries).then(list => {
     for (let item of list) {
