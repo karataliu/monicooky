068ef6d8a3888ac37aaf5757a9bbee715aa175eb
Dong Liu <doliu@microsoft.com>
2016-06-08 16:44:27 +0800

    conf and shell update.

--- report.sh
+++ report.sh
@@ -2,5 +2,5 @@
 
 NODE_BIN=/usr/sbin/node
 CUR=$(dirname $0)
-$NODE_BIN $CUR/src/run.js| zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -i -
+$NODE_BIN $CUR/src/run.js query -s $@| zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -i -
 
