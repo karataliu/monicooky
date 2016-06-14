feafbc017266bcaac6fa9c4524c1f1c24d808716
Dong Liu <doliu@microsoft.com>
2016-06-14 14:32:17 +0800

    Support single get query.

--- src/run.ts
+++ src/run.ts
@@ -2,15 +2,18 @@ import mcClient = require('./mcClient');
 import { McLib } from './mcLib';
 import { McUtil } from './mcUtil';
 import { McAdapter } from './adapter';
-import { McClientOptions, IMcQueryEntry } from './common';
+import { McClientOptions, IMcQueryEntry, IMcClient } from './common';
 
 const argv = require('minimist')(process.argv.slice(2));
 const confPath = argv['c'] || __dirname + '/conf.json';
 const queryTemplatePath = argv['t'] || __dirname + '/query.json';
+const url = argv['u'];
 let action = 'help';
 if (argv['_'].length > 0) {
     action = argv['_'][0];
 }
+
+let client: IMcClient;
 
 switch (action) {
     case 'discovery':
@@ -32,15 +35,22 @@ switch (action) {
             }
         }).catch(console.error);
         break;
+    case 'get':
+        if (!url) {
+            console.log("url must be set.");
+        }
+        getAdapter();
+        client.get(url).then(console.log).catch(console.error);
+        break;
     case 'help':
     default:
-        console.log("node run.ts <help|discovery|query> [-c conf.json] [-q queryTempalte.json] [-s subsId1,subsId2]");
+        console.log("node run.js <help|discovery|query|get> [-c conf.json] [-q queryTempalte.json] [-s subsId1,subsId2] [-u url]");
 }
 
 function getAdapter(): McAdapter {
     const conf: any = McUtil.LoadFile(confPath);
     const options: McClientOptions = conf;
-    const client = new mcClient.McClient(options);
+    client = new mcClient.McClient(options);
     const mcLib = new McLib(client);
     return new McAdapter(mcLib);
 }
